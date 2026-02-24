fb6108cc2654b0d9c91663f3b3cd31350e2de7b6
Taher Sohagpurwala <tahersohagpurwala@gmail.com>
2026-02-24 08:56:58 +0530

    Added hobbies section

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '''
-    <h1>Name: John Doe</h1>
-    <h2>AppID: APP-2024-001</h2>
+    <h1>Name: Taher Sohagpurwala</h1>
+    <h2>AppID: 2409540</h2>
+    <h3>Hobbies:</h3>
+    <ul>
+        <li>Coding</li>
+        <li>Reading</li>
+        <li>Gaming</li>
+    </ul>
     '''
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
